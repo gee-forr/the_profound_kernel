3f002908759ce87bfdf0924b2e6bff5ea1e032d7
Gabriel Fortuna <gee.forr@gmail.com>
2013-01-27 11:58:56 +0200

    Remove debug statements, add last_known_offender

--- lib/the_profound_kernel.rb
+++ lib/the_profound_kernel.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "bundler/setup"
 Bundler.require(:default)
 
-require 'pry'
+require "pry"
 
 module ProfoundKernel
   class << self
@@ -22,6 +22,8 @@ module ProfoundKernel
   end
 
   class Processor
+    LAST_TWEET_KEY = 'last_corrected_tweet'
+
     def initialize
       Twitter.configure do |config|
         config.consumer_key       = ProfoundKernel.configuration.consumer_key
@@ -34,7 +36,7 @@ module ProfoundKernel
     end
 
     def run_search
-      Twitter.search(ProfoundKernel.configuration.wrong_phrase, :count => 32, :result_type => "recent").results
+      Twitter.search(ProfoundKernel.configuration.wrong_phrase, :result_type => "recent").results
     end
 
     def sanitize_search(search_results)
@@ -44,40 +46,30 @@ module ProfoundKernel
         .reject { |status| status.text =~ ProfoundKernel.configuration.right_phrase_regex } # Reject tweets that also mention deep seated, it might be a correction already
     end
 
-    def validate_tweet(tweet)
-      # Should return yay if we can correct or nay if we shouldnt, based on validity
-      return false if tweet.text =~ /^RT\s/
-      puts "This is not an RT : [#{tweet.text}]"
-
+    def should_correct?(tweet) # This needs refactoring
+      return false if last_known_correction >= tweet.id # No, if this is an old tweet we've already processed
+      return false if tweet.text =~ /^RT\s/             # No, if this is an old school retweet
+      return false if tweet.retweet?                    # No, if this is a new school retweet
       return false if recent_offender?(tweet.from_user)
-      puts "This is not a recent offender [#{tweet.from_user}]"
 
       tweet.user_mentions.each do |mentioned| 
-        if recent_offender?(mentioned.screen_name)  # Test for mentioner abuse
-          puts "this mentioned is a recent offender, moving on  [#{mentioned.screen_name}]"
-          return false
-        else
-          puts "[#{mentioned.screen_name}] is not a recent offender"
-        end
+        return false if recent_offender?(mentioned.screen_name)  # Test for mentioner abuse
       end
 
-      puts "This is a valid tweet:"
-      true
+      true # Passes all checks
     end
 
     def tweet_correction(tweet)
-      # construct message
       msg = "@#{tweet.from_user} I think you mean #{ProfoundKernel.configuration.right_phrase}."
 
-      # send message
       if ProfoundKernel.configuration.dry_run
         puts "Replying to [#{tweet.id}] [#{tweet.text}] - [#{msg}]"
-        
       else
         Twitter.update(msg, {in_reply_to_status_id: tweet.id})
       end
 
       update_offenders_list(tweet.from_user)
+      update_last_known_correction(tweet.id)
     end
 
     def recent_offender?(user)
@@ -86,12 +78,20 @@ module ProfoundKernel
 
     def offender_time(user)
       time = @redis.get(user)
-      
+
       time.nil? ? 1.year.ago: time.to_time
     end
 
     def update_offenders_list(user)
       @redis.set user, Time.now
+    end
+
+    def update_last_known_correction(id)
+      @redis.set LAST_TWEET_KEY, id
+    end
+
+    def last_known_correction
+      @redis.get(LAST_TWEET_KEY).to_i or 1
     end
   end
 end
